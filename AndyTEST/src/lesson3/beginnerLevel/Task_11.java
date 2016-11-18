ec6b4c2ac4710e48a160bd47e5a3107afaaec76c
Avedon-007 <a_n_d_y-007@mail.ru>
2016-11-18 15:42:33 +0200

    Finish of Task 11 of lesson 3 Beginner level.

--- AndyTEST/src/lesson3/beginnerLevel/Task_11.java
+++ AndyTEST/src/lesson3/beginnerLevel/Task_11.java
@@ -17,43 +17,45 @@ public class Task_11 {
 		{
 			if(directionFlag == true) 
 			{
-				xPosition++;
-				yPosition--;
+				xPosition--;
+				yPosition++;
 			}
 			else 
 			{
-				xPosition--;
-				yPosition++;
-			}
-			
-			if((directionFlag = true) && (yPosition< 0)) // upper border
-			{
-				yPosition++;
-				directionFlag = false;
-				
-			}
-			if((directionFlag = true) && (xPosition> dimension-1))	// right wall(border)
-			{
-				xPosition--;
-				yPosition+=2;
-				directionFlag = false;
-			}
-			if((directionFlag = false) && (yPosition > dimension-1)) // bottom border
-			{
+				xPosition++;
 				yPosition--;
-				directionFlag = true;
-				
 			}
-			if((directionFlag = false) && (xPosition < 0))	// left wall(border)
+			if((directionFlag == true) && (yPosition > dimension-1))	// right wall(border)
+			{
+				xPosition+=2;
+				yPosition--;
+				directionFlag = false;
+			}
+			else if((directionFlag == true) && (xPosition < 0)) // upper border
 			{
 				xPosition++;
-				yPosition-=2;
+				directionFlag = false;
+				
+			}
+			
+			else if((directionFlag == false) && (xPosition > dimension-1)) // bottom border
+			{
+				yPosition+=2;
+				xPosition--;
+				directionFlag = true;
+				
+			}
+			else if((directionFlag == false) && (yPosition < 0))	// left wall(border)
+			{
+				
+				yPosition++;
 				directionFlag = true;
 			}
+		/*   //  Bebug
 			System.out.println("current number is " + i);
 			System.out.println("xPosition=" + xPosition);
 			System.out.println("yPositiony=" + yPosition);
-
+		*/
 			myArray[xPosition][yPosition] = i;
 		}
 		
@@ -79,7 +81,96 @@ public class Task_11 {
 	public static void main(String[] args) 
 	{
 		Task_11 superArray = new Task_11();
-		superArray.showArray(superArray.createArray(10));
+		superArray.showArray(superArray.createArray(4));
 	}
 
 }
+
+
+/*
+
+public class Task_11 {
+
+
+public int[][] createArray(int dimension)
+{
+	int[][] myArray = new int[dimension][dimension];
+	boolean directionFlag = true;
+	int xPosition = 1;
+	int yPosition = 0;
+
+	myArray[0][0] = 1;
+	myArray[1][0] = 2;
+
+	for(int i = 3; i <= dimension*dimension; i++)
+	{
+		if(directionFlag == true) 
+		{
+			xPosition--;
+			yPosition++;
+		}
+		else 
+		{
+			xPosition++;
+			yPosition--;
+		}
+		
+		if((directionFlag = true) && (xPosition< 0)) // upper border
+		{
+			xPosition++;
+			directionFlag = false;
+			
+		}
+		else if((directionFlag = true) && (yPosition> dimension-1))	// right wall(border)
+		{
+			xPosition+=2;
+			yPosition--;
+			directionFlag = false;
+		}
+		else if((directionFlag = false) && (xPosition > dimension-1)) // bottom border
+		{
+			yPosition+=2;
+			xPosition--;
+			directionFlag = true;
+			
+		}
+		else if((directionFlag = false) && (yPosition < 0))	// left wall(border)
+		{
+			
+			yPosition++;
+			directionFlag = true;
+		}
+		System.out.println("current number is " + i);
+		System.out.println("xPosition=" + xPosition);
+		System.out.println("yPositiony=" + yPosition);
+
+		myArray[xPosition][yPosition] = i;
+	}
+	
+	
+	return myArray;
+}
+
+public void showArray(int[][] myArray)
+{
+	for(int x[]:myArray)
+	{
+		for(int y: x)
+		{
+			//System.out.print(y + " ");
+		}
+		//System.out.println();
+	}
+}
+
+
+
+
+public static void main(String[] args) 
+{
+	Task_11 superArray = new Task_11();
+	superArray.showArray(superArray.createArray(10));
+}
+
+}
+*/
