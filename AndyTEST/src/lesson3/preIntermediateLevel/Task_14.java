31b2c4fe14556b528c27752582b4fcfeee1efff1
Avedon-007 <a_n_d_y-007@mail.ru>
2016-11-28 14:36:25 +0200

    Final fix of Task 14 of Lesson 3 pre-intermediate level.

--- AndyTEST/src/lesson3/preIntermediateLevel/Task_14.java
+++ AndyTEST/src/lesson3/preIntermediateLevel/Task_14.java
@@ -10,7 +10,7 @@ Output: T
 package lesson3.preIntermediateLevel;
 
 
-public class Task_14 	// ¬ывести слово в диагональной матрице
+public class Task_14
 {	
 	public static void main(String[] args) 
 	{		
@@ -19,41 +19,20 @@ public class Task_14 	// ¬ывести слово в диагональной матрице
 		//INstantiate new object
 		Task_14 task = new Task_14();
 		//Method application
-		//task.createDiagonalMatrix(word);
-		task.showArray(task.createDiagonalMatrix(word));
+		task.createDiagonalMatrix(word);
 	}	
 		
-		
-			
-}
-
-/*
-
-public class Task_14 	// ¬ывести слово в диагональной матрице
-{
-	private static Scanner sc1;
-	
-	public static void main(String[] args) 
-	{		
-		String word;
-		char[] arrayWord;
-		String s = "  ";
+	public void createDiagonalMatrix(String word)
+	{
+		char[] arrayWord = word.toCharArray();		
+		String s = " ";
 		String s1 = "";
-		
-		System.out.print("Enter the word: ");
-		sc1 = new Scanner(System.in);
-		word = sc1.nextLine();
-		
-		arrayWord = word.toCharArray();		
-		
 		System.out.println(arrayWord[0]);
 		
 		for(int i = 1; i < arrayWord.length; i++)
 		{
 			s1 = s1 + s;			
 			System.out.println(s1 + arrayWord[i]);
-		}					
-	}
+		}	
+	}			
 }
-
-*/
