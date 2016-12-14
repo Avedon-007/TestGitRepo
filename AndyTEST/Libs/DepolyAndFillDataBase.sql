436feab1e62c2cc31ab3eb43d546ad5201d18a11
Avedon-007 <a_n_d_y-007@mail.ru>
2016-12-14 15:38:09 +0200

    Created one sql file which combine all sql queries.

--- AndyTEST/Libs/DepolyAndFillDataBase.sql
+++ AndyTEST/Libs/DepolyAndFillDataBase.sql
@@ -46,7 +46,7 @@ CREATE TABLE Airports
 (
 	
 airportNumber int NOT NULL PRIMARY KEY, /* I've added this colomn for relation between such tables: Airports & Flights*/
-	airport varchar(20) NOT NULL,
+airport varchar(20) NOT NULL,
 	
 dutyFree varchar(3) NOT NULL,
 	
--- AndyTEST/Libs/JOIN.sql
+++
@@ -1,8 +0,0 @@
-Use TestDB
-GO
-
-SELECT airport, flightNumber, departureAirport, arrivalAirport, stopsNumber, priorityBoarding, dutyFree, availableSeats, averageTicketPrice
-FROM Airports
-INNER JOIN Flights
-ON Airports.airportNumber = Flights.airportNumber
-GO
