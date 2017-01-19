b4248f09edd2c2dd46ef4ef226c370bbb4a7e651
Avedon-007 <A_N_D_Y-007@mail.ru>
2017-01-19 19:20:00 +0200

    Temporary file to work with ResultSet

---
+++ AndyTEST/src/lesson4/test/ExecuteQueryAndGenerateCSV.java
@@ -0,0 +1,53 @@
+package lesson4.test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ExecuteQueryAndGenerateCSV {
+
+	static final String DATABASE_DRIVER = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+	static final String DATABASE_URL = "jdbc:sqlserver://localhost:1433;";
+	static final String DATABASE_NAME = "databaseName = TestDB;";
+	static final String DB_SECURITY = "integratedSecurity=true;";
+	static String query = "Use TestDB SELECT * FROM Airports";
+	
+	public static void main(String[] args) throws Exception, SQLException 
+	{
+		ExecuteQueryAndGenerateCSV myObject = new ExecuteQueryAndGenerateCSV();
+		System.out.println(myObject.ResultSetToString(myObject.getResultSetFromQuery(query)));
+
+	}
+	
+	private ResultSet getResultSetFromQuery(String query) throws ClassNotFoundException, SQLException
+	{
+		ResultSet result;
+		Class.forName(DATABASE_DRIVER);
+		Connection myConn = DriverManager.getConnection(DATABASE_URL + DATABASE_NAME + DB_SECURITY);
+		Statement myStmt = myConn.createStatement();
+		result = myStmt.executeQuery(query);
+		return result;		
+	}
+	private String ResultSetToString(ResultSet result) throws SQLException 
+	{
+		String resultString = "";
+		
+		ResultSetMetaData rsmd = result.getMetaData();
+		int columnCount = rsmd.getColumnCount();
+		while(result.next())
+		{
+		// The column count starts from 1
+			for (int i = 1; i <= columnCount; i++ ) {
+		 
+				resultString += result.getString(rsmd.getColumnName(i)) + ";";
+		  
+			}
+		}
+		
+		
+		return resultString;
+	}
+}
