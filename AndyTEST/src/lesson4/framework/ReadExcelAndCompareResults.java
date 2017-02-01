d58f66b4764793b1a1892eb09e1843bd03b10b30
Avedon-007 <A_N_D_Y-007@mail.ru>
2017-02-01 17:26:40 +0200

    Refactoring of Lesson 4

--- AndyTEST/src/lesson4/framework/ReadExcelAndCompareResults.java
+++ AndyTEST/src/lesson4/framework/ReadExcelAndCompareResults.java
@@ -16,7 +16,7 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import lesson4.ExecuteQueryAndGenerateCSV;
+
 
 public class ReadExcelAndCompareResults
 {	
