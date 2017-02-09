48506b789613f64f5ed1ef14a147953c41694b36
Avedon-007 <A_N_D_Y-007@mail.ru>
2017-02-09 10:43:34 +0200

    Frefactoring of Framework of Lesson 4 for MySQL
    Has added task description.

--- AndyTEST/src/lesson4/framework/Runner.java
+++ AndyTEST/src/lesson4/framework/Runner.java
@@ -7,6 +7,11 @@ public class Runner
 {
 	public static void main(String[] args) throws IOException, ClassNotFoundException, SQLException, InterruptedException 
 	{
+		System.out.println("We need to test flight tickets booking system. User can browse and choose flights, tickets' availability," 
+							+ " additional services and information via web front-end application. All necessary data are stored in" 
+							+ " the three tables of database (airports, flights, airlines) and are updated in realtime. User performs" 
+							+ " its requests without reference to a specific date and gets current state of the system. We need to"
+							+ " test behavior and correct work of the system.\n");
 		System.out.println("GETTING STARTED...");
 		
 		TestEnvironment myInstance = new TestEnvironment();
